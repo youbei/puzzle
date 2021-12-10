e3a18be8ebc1a4627455d6f6e85903eb7037621a
Youbei <youbei@YoubeideMac-mini.local>
2021-12-10 15:20:00 +0800

    update

--- src/pages/CropImages/index.js
+++ src/pages/CropImages/index.js
@@ -5,11 +5,85 @@ import Container from '../../components/Container'
 
 function Index() {
 	return (
-		<div className="merge-images">
+		<div className="crop-images">
 			<Header />
 			<Container>
-				<div className="merge-images-container">
-					
+				<div className="crop-images-container">
+					<section className="advice">
+						<h1 className="advice__title">建设中。</h1>
+					</section>
+					<section className="city-stuff">
+						<ul className="skyscrappers__list">
+							<li className="skyscrapper__item skyscrapper-1"></li>
+							<li className="skyscrapper__item skyscrapper-2"></li>
+							<li className="skyscrapper__item skyscrapper-3"></li>
+							<li className="skyscrapper__item skyscrapper-4"></li>
+							<li className="skyscrapper__item skyscrapper-5"></li>
+						</ul>
+						<ul className="tree__container">
+							<li className="tree__list">
+								<ul className="tree__item tree-1">
+									<li className="tree__trunk"></li>
+									<li className="tree__leaves"></li>
+								</ul>
+								<ul className="tree__item tree-2">
+									<li className="tree__trunk"></li>
+									<li className="tree__leaves"></li>
+								</ul>
+								<ul className="tree__item tree-3">
+									<li className="tree__trunk"></li>
+									<li className="tree__leaves"></li>
+								</ul>  
+								<ul className="tree__item tree-4">
+									<li className="tree__trunk"></li>
+									<li className="tree__leaves"></li>
+								</ul>  
+								<ul className="tree__item tree-5">
+									<li className="tree__trunk"></li>
+									<li className="tree__leaves"></li>
+								</ul>  
+								<ul className="tree__item tree-6">
+									<li className="tree__trunk"></li>
+									<li className="tree__leaves"></li>
+								</ul>  
+								<ul className="tree__item tree-7">
+									<li className="tree__trunk"></li>
+									<li className="tree__leaves"></li>
+								</ul>  
+								<ul className="tree__item tree-8">
+									<li className="tree__trunk"></li>
+									<li className="tree__leaves"></li>
+								</ul>  
+							</li>
+						</ul>
+						<ul className="crane__list crane-1">
+							<li className="crane__item crane-cable crane-cable-1"></li>
+							<li className="crane__item crane-cable crane-cable-2"></li>
+							<li className="crane__item crane-cable crane-cable-3"></li>
+							<li className="crane__item crane-stand"></li>
+							<li className="crane__item crane-weight"></li>
+							<li className="crane__item crane-cabin"></li>
+							<li className="crane__item crane-arm"></li>
+						</ul>
+						<ul className="crane__list crane-2">
+							<li className="crane__item crane-cable crane-cable-1"></li>
+							<li className="crane__item crane-cable crane-cable-2"></li>
+							<li className="crane__item crane-cable crane-cable-3"></li>
+							<li className="crane__item crane-stand"></li>
+							<li className="crane__item crane-weight"></li>
+							<li className="crane__item crane-cabin"></li>
+							<li className="crane__item crane-arm"></li>
+						</ul>
+						<ul className="crane__list crane-3">
+							<li className="crane__item crane-cable crane-cable-1"></li>
+							<li className="crane__item crane-cable crane-cable-2"></li>
+							<li className="crane__item crane-cable crane-cable-3"></li>
+							<li className="crane__item crane-stand"></li>
+							<li className="crane__item crane-weight"></li>
+							<li className="crane__item crane-cabin"></li>
+							<li className="crane__item crane-arm"></li>
+						</ul>
+					</section>
 				</div>
 			</Container>
 		</div>
--- src/pages/MergeImages/index.js
+++ src/pages/MergeImages/index.js
@@ -5,6 +5,7 @@ import Container from '../../components/Container'
 
 const TOTAL_WIDTH = 750
 const COLUMN = 3 
+const PADDING = 10 
 
 class Index extends React.Component {
 	constructor(props) {
@@ -12,23 +13,24 @@ class Index extends React.Component {
 		this.state = {
 			isDropOver: false, 
 			totalWidth: TOTAL_WIDTH,
-			column: COLUMN
+			column: COLUMN,
+			padding: PADDING
 		}
+		this.imageList = []
 	}
 
 	dropHandler(e) {
 		e.preventDefault()
 		this.setState({isDropOver: false})
+		this.imageList = []
 
-		const {totalWidth, column} = this.state
-		if(Number(totalWidth) < 1 || Number(totalWidth) > 9999 || Number(column) < 1 || Number(column) > 9) {
+		if(!this.validate()) {
 			this.reset()
 			return
 		}
 
 		this.removeCanvas()
 
-		const imageList = []
 		const length = e.dataTransfer.items.length
 		for (let i = 0; i < length; i++) {
 			if (e.dataTransfer.items[i].kind === 'file' && e.dataTransfer.items[i].type.match('^image/')) {
@@ -39,11 +41,9 @@ class Index extends React.Component {
 					const img = new Image()
 					img.src = reader.result
 					img.onload = () => {
-						imageList.push(img)
+						this.imageList.push(img)
 						if (i === length -1) {
-							const minWidth = imageList.reduce((prev, current) => (prev.width < current.width) ? prev : current)
-							const minHeight = imageList.reduce((prev, current) => (prev.height < current.height) ? prev : current)
-							this.merge(imageList, minWidth.width, minHeight.height)
+							this.merge()
 						}
 					}
 				}, false)
@@ -51,35 +51,68 @@ class Index extends React.Component {
 		}
 	}
 
-	merge(imageList, width, height) {
-		const {totalWidth, column} = this.state
+	merge() {
+		if (this.imageList.length === 0) {
+			return
+		}
+
+		if (!this.validate()) {
+			return
+		}
+
+		const totalWidth = Number(this.state.totalWidth)
+		const column = Number(this.state.column)
+		const padding = Number(this.state.padding)
+
+		// 找出所有图片中的最小宽度和高度
+		const width = this.imageList.reduce((prev, current) => (prev.width < current.width) ? prev : current).width
+		const height = this.imageList.reduce((prev, current) => (prev.height < current.height) ? prev : current).height
+
+		// 计算有多少行
+		const row = Math.ceil(this.imageList.length / column)
+
+		// 调整后的图片尺寸
+		const adjustedWidth = (totalWidth - (column - 1) * padding) / column
+		const adjustedHeight = adjustedWidth * height / width 
+
 		const canvas = document.getElementById('canvas')
 		const context = canvas.getContext('2d')
-		const adjustedWidth = totalWidth / column
-		const adjustedHeight = adjustedWidth * height / width 
-		const row = Math.ceil(imageList.length / column)
 		canvas.width = totalWidth 
-		canvas.height = adjustedHeight * row
+		canvas.height = adjustedHeight * row + padding * (row - 1)
 
-		imageList.forEach((item, i) => {
-			console.log(i)
+		this.imageList.forEach((item, i) => {
 			context.drawImage(
 				item, 
 				0, // x轴，从左侧多少像素开始裁切图片 
 				0, // y轴，从顶部多少像素开始裁切图片 
 				width, // 裁切的图片的宽度 
 				height, // 裁切图片的高度 
-				i % column * adjustedWidth, // x轴，当前图片离左侧多少像素
-				Math.floor(i / column) * adjustedHeight, // y轴，当前图片离顶部多少像素
+				i % column * (adjustedWidth + padding), // x轴，当前图片离左侧多少像素
+				Math.floor(i / column) * (adjustedHeight + padding), // y轴，当前图片离顶部多少像素
 				adjustedWidth, // 图片宽度缩小至多少像素 
 				adjustedHeight // 图片高度缩小至多少像素
 			)
 		})
 	}
 
+	validate() {
+		const {totalWidth, column, padding} = this.state
+		if(
+			Number(totalWidth) < 1 
+			|| Number(totalWidth) > 9999 
+			|| Number(column) < 1 
+			|| Number(column) > 9
+			|| Number(padding) < 0
+			|| Number(padding) > 20 
+		) {
+			return false
+		}
+
+		return true
+	}
+
 	reset() {
-		this.removeCanvas()
-		this.setState({totalWidth: TOTAL_WIDTH, column: COLUMN})
+		this.setState({totalWidth: TOTAL_WIDTH, column: COLUMN, padding: PADDING}, () => this.merge())
 	}
 
 	removeCanvas() {
@@ -99,7 +132,7 @@ class Index extends React.Component {
 	}
 
 	render() {
-		const {isDropOver, totalWidth, column} =this.state
+		const {isDropOver, totalWidth, column, padding} =this.state
 		return (
 			<div className="merge-images">
 				<Header />
@@ -114,20 +147,24 @@ class Index extends React.Component {
 									<p>图片宽度</p>
 									<input 
 										type='number'
-										min='1'
-										max='9999'
 										value={totalWidth}
-										onChange={(e) => {this.setState({totalWidth: e.target.value})}}
+										onChange={(e) => {this.setState({totalWidth: e.target.value}, () => this.merge())}}
 									/>
 								</div>
 								<div className="merge-images-container-right-top-item">
 									<p>每行个数</p>
 									<input 
 										type='number'
-										min='1'
-										max='9'
 										value={column}
-										onChange={(e) => {this.setState({column: e.target.value})}}
+										onChange={(e) => {this.setState({column: e.target.value}, () => this.merge())}}
+									/>
+								</div>
+								<div className="merge-images-container-right-top-item">
+									<p>图片间隔</p>
+									<input 
+										type='number'
+										value={padding}
+										onChange={(e) => {this.setState({padding: e.target.value}, () => this.merge())}}
 									/>
 								</div>
 								<button 
