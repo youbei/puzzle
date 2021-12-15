8db2a07a9bdbe722ef598db2308de11ece6e45da
Youbei <youbei@YoubeideMac-mini.local>
2021-12-15 16:21:16 +0800

    update

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -2,15 +2,13 @@ import './index.scss'
 import React from 'react'
 import Nav from '../Nav'
 
-class Index extends React.Component {
-	render() {
-		return (
-			<div className="container">
-				<Nav />
-				{this.props.children}
-			</div>
-		) 
-	}
+function Index(props) {
+	return (
+		<div className="container">
+			<Nav />
+			{props.children}
+		</div>
+	) 
 }
 
 export default Index
--- src/pages/MergeImages/index.js
+++ src/pages/MergeImages/index.js
@@ -1,109 +1,113 @@
 import './index.scss'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Header from '../../components/Header'
 import Container from '../../components/Container'
 
 const TOTAL_WIDTH = 750
 const COLUMN = 3 
 const PADDING = 10 
+let imageList = [] 
 
-class Index extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			isDropOver: false, 
-			totalWidth: TOTAL_WIDTH,
-			column: COLUMN,
-			padding: PADDING
-		}
-		this.imageList = []
-	}
+function Index() {
+	const [isDropOver, setIsDropOver] = useState(false)
+	const [totalWidth, setTotalWidth] = useState(TOTAL_WIDTH)
+	const [column, setColumn] = useState(COLUMN)
+	const [padding, setPadding] = useState(PADDING)
+	useEffect(() => merge(), [totalWidth, column, padding])
 
-	dropHandler(e) {
+	function dropHandler(e) {
 		e.preventDefault()
-		this.setState({isDropOver: false})
-		this.imageList = []
+		setIsDropOver(false)
+		imageList = [] 
+		let p = []
 
-		if(!this.validate()) {
-			this.reset()
+		if(!validate()) {
+			reset()
 			return
 		}
 
-		this.removeCanvas()
+		removeCanvas()
 
-		const length = e.dataTransfer.items.length
-		for (let i = 0; i < length; i++) {
+		for (let i = 0; i < e.dataTransfer.items.length; i++) {
 			if (e.dataTransfer.items[i].kind === 'file' && e.dataTransfer.items[i].type.match('^image/')) {
 				const file = e.dataTransfer.items[i].getAsFile()
-				const reader = new FileReader()
-				reader.readAsDataURL(file)
-				reader.addEventListener('load', () => {
-					const img = new Image()
-					img.src = reader.result
-					img.onload = () => {
-						this.imageList.push(img)
-						if (i === length -1) {
-							this.merge()
-						}
-					}
-				}, false)
+				p.push(processImages(file))
 			}
 		}
+
+		Promise.all(p).then((list) => {
+			imageList = list 
+			merge()
+		})
 	}
 
-	merge() {
-		if (this.imageList.length === 0) {
+	function processImages(file) {
+		return new Promise(resolve => {
+			const reader = new FileReader()
+			reader.addEventListener('load', () => {
+				const img = new Image()
+				img.src = reader.result
+				img.addEventListener('load', () => {
+					resolve(img)
+				})
+			}, false)
+			reader.readAsDataURL(file)
+		})
+	}
+
+	function merge() {
+		console.log(imageList)
+		if (imageList.length === 0) {
 			return
 		}
 
-		if (!this.validate()) {
+		if (!validate()) {
 			return
 		}
 
-		const totalWidth = Number(this.state.totalWidth)
-		const column = Number(this.state.column)
-		const padding = Number(this.state.padding)
+		const tw = Number(totalWidth)
+		const c = Number(column)
+		const p = Number(padding)
 
 		// 找出所有图片中的最小宽度和高度
-		const width = this.imageList.reduce((prev, current) => (prev.width < current.width) ? prev : current).width
-		const height = this.imageList.reduce((prev, current) => (prev.height < current.height) ? prev : current).height
+		const width = imageList.reduce((prev, current) => (prev.width < current.width) ? prev : current).width
+		const height = imageList.reduce((prev, current) => (prev.height < current.height) ? prev : current).height
 
 		// 计算有多少行
-		const row = Math.ceil(this.imageList.length / column)
+		const row = Math.ceil(imageList.length / c)
 
 		// 调整后的图片尺寸
-		const adjustedWidth = (totalWidth - (column - 1) * padding) / column
+		const adjustedWidth = (tw - (c - 1) * p) / c
 		const adjustedHeight = adjustedWidth * height / width 
 
 		const canvas = document.getElementById('canvas')
 		const context = canvas.getContext('2d')
-		canvas.width = totalWidth 
-		canvas.height = adjustedHeight * row + padding * (row - 1)
+		canvas.width = tw
+		canvas.height = adjustedHeight * row + p * (row - 1)
 
-		this.imageList.forEach((item, i) => {
+		imageList.forEach((item, i) => {
 			context.drawImage(
 				item, 
 				0, // x轴，从左侧多少像素开始裁切图片 
 				0, // y轴，从顶部多少像素开始裁切图片 
 				width, // 裁切的图片的宽度 
 				height, // 裁切图片的高度 
-				i % column * (adjustedWidth + padding), // x轴，当前图片离左侧多少像素
-				Math.floor(i / column) * (adjustedHeight + padding), // y轴，当前图片离顶部多少像素
+				i % c * (adjustedWidth + p), // x轴，当前图片离左侧多少像素
+				Math.floor(i / c) * (adjustedHeight + p), // y轴，当前图片离顶部多少像素
 				adjustedWidth, // 图片宽度缩小至多少像素 
 				adjustedHeight // 图片高度缩小至多少像素
 			)
 		})
 	}
 
-	validate() {
-		const {totalWidth, column, padding} = this.state
+	function validate() {
 		if(
 			Number(totalWidth) < 1 
-			|| Number(totalWidth) > 9999 
-			|| Number(column) < 1 
-			|| Number(column) > 9
-			|| Number(padding) < 0
-			|| Number(padding) > 20 
+		|| Number(totalWidth) > 9999 
+		|| Number(column) < 1 
+		|| Number(column) > 9
+		|| Number(padding) < 0
+		|| Number(padding) > 20 
 		) {
 			return false
 		}
@@ -111,82 +115,81 @@ class Index extends React.Component {
 		return true
 	}
 
-	reset() {
-		this.setState({totalWidth: TOTAL_WIDTH, column: COLUMN, padding: PADDING}, () => this.merge())
+	function reset() {
+		setTotalWidth(TOTAL_WIDTH)
+		setColumn(COLUMN)
+		setPadding(PADDING)
 	}
 
-	removeCanvas() {
+	function removeCanvas() {
 		const canvas = document.getElementById('canvas')
 		const context = canvas.getContext('2d')
 		context.clearRect(0, 0, canvas.width, canvas.height)
 	}
 
-	dragOverHandler(e) {
-		this.setState({isDropOver: true})
+	function dragOverHandler(e) {
+		setIsDropOver(true)
 		e.preventDefault()
 	}
 
-	dragLeaveHandler(e) {
-		this.setState({isDropOver: false})
+	function dragLeaveHandler(e) {
+		setIsDropOver(false)
 		e.preventDefault()
 	}
 
-	render() {
-		const {isDropOver, totalWidth, column, padding} =this.state
-		return (
-			<div className="merge-images">
-				<Header />
-				<Container>
-					<div className="merge-images-container">
-						<div className="merge-images-container-left">
-							<canvas id="canvas"></canvas>
+	return (
+		<div className="merge-images">
+			<Header />
+			<Container>
+				<div className="merge-images-container">
+					<div className="merge-images-container-left">
+						<canvas id="canvas"></canvas>
+					</div>
+					<div className="merge-images-container-right">
+						<div className="merge-images-container-right-top">
+							<div className="merge-images-container-right-top-item">
+								<p>图片宽度</p>
+								<input 
+									type='number'
+									value={totalWidth}
+									onChange={(e) => setTotalWidth(e.target.value) }
+								/>
+							</div>
+							<div className="merge-images-container-right-top-item">
+								<p>每行个数</p>
+								<input 
+									type='number'
+									value={column}
+									onChange={(e) => setColumn(e.target.value)}
+								/>
+							</div>
+							<div className="merge-images-container-right-top-item">
+								<p>图片间隔</p>
+								<input 
+									type='number'
+									value={padding}
+									onChange={(e) => setPadding(e.target.value)}
+								/>
+							</div>
+							<button 
+								onClick={() => reset()}
+								className="merge-images-container-right-top-item"
+							>重置</button>
 						</div>
-						<div className="merge-images-container-right">
-							<div className="merge-images-container-right-top">
-								<div className="merge-images-container-right-top-item">
-									<p>图片宽度</p>
-									<input 
-										type='number'
-										value={totalWidth}
-										onChange={(e) => {this.setState({totalWidth: e.target.value}, () => this.merge())}}
-									/>
-								</div>
-								<div className="merge-images-container-right-top-item">
-									<p>每行个数</p>
-									<input 
-										type='number'
-										value={column}
-										onChange={(e) => {this.setState({column: e.target.value}, () => this.merge())}}
-									/>
-								</div>
-								<div className="merge-images-container-right-top-item">
-									<p>图片间隔</p>
-									<input 
-										type='number'
-										value={padding}
-										onChange={(e) => {this.setState({padding: e.target.value}, () => this.merge())}}
-									/>
-								</div>
-								<button 
-									onClick={() => this.reset()}
-									className="merge-images-container-right-top-item"
-								>重置</button>
-							</div>
-							<div 
-								className="merge-images-container-right-bottom"
-								style={isDropOver ? { backgroundColor: 'rgba(170, 172, 247,.8)', color: '#fff'} : {}}
-								onDrop={(e) => this.dropHandler(e)} 
-								onDragOver={(e) => this.dragOverHandler(e)}
-								onDragLeave={(e) => this.dragLeaveHandler(e)}
-							>
-								<h1>把图片拖到这里</h1>
-							</div>
+						<div 
+							className="merge-images-container-right-bottom"
+							style={isDropOver ? { backgroundColor: 'rgba(170, 172, 247,.8)', color: '#fff'} : {}}
+							onDrop={(e) => dropHandler(e)} 
+							onDragOver={(e) => dragOverHandler(e)}
+							onDragLeave={(e) => dragLeaveHandler(e)}
+						>
+							<h1>把图片拖到这里</h1>
 						</div>
 					</div>
-				</Container>
-			</div>
-		)
-	}
+				</div>
+			</Container>
+		</div>
+	)
 }
 
 export default Index
